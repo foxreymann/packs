3b6e72b97498b281c6d021899cf5e5d7b6703611
tomaszjureczko <tomasz.jureczko@gmail.com>
2013-11-20 13:51:25 +0000

    local js

--- js/packs.js
+++ js/packs.js
@@ -1,55 +1,55 @@
 function Packs(data) {
-    this.data = data;
+  this.data = data;
 
-    this.template = _.template($('#packtpl').text());
+  this.template = _.template($('#packtpl').text());
 }
 
 Packs.prototype.init = function() {
-    jQuery('#sort li a').on('click', this.changeSort.bind(this));
-    jQuery(this.buildPackUI.bind(this, this.data));
-    
-    return this;
+  jQuery('#sort li a').on('click', this.changeSort.bind(this));
+  jQuery(this.buildPackUI.bind(this, this.data));
+  
+  return this;
 };
 
 Packs.prototype.buildPackUI = function(packData) {
-    var $ = jQuery.noConflict();
+  var $ = jQuery.noConflict();
 
-    $('#packs *').remove();
+  $('#packs *').remove();
 
-    packData.packs.forEach(function(pack) {
-        $('#packs').append(this.template(pack));
-    }.bind(this));
+  packData.packs.forEach(function(pack) {
+      $('#packs').append(this.template(pack));
+  }.bind(this));
 };
 
 Packs.prototype.sortByFieldCallback = function(sortField, sortType, a, b) {
-    if (sortType === 'number') {
-        return parseInt(a[sortField], 10) - parseInt(b[sortField], 10);
-    } else if (sortType === 'string') {
-        return a[sortField].localeCompare(b[sortField]);
-    }
+  if (sortType === 'number') {
+      return parseInt(a[sortField], 10) - parseInt(b[sortField], 10);
+  } else if (sortType === 'string') {
+      return a[sortField].localeCompare(b[sortField]);
+  }
 };
 
 Packs.prototype.sortPacks = function(sortField, sortType) {
-    var newData = JSON.parse(JSON.stringify(this.data)); // Clone the data in a simple manner
+  var newData = JSON.parse(JSON.stringify(this.data)); // Clone the data in a simple manner
 
-    newData.packs.sort(this.sortByFieldCallback.bind(this, sortField, sortType));
+  newData.packs.sort(this.sortByFieldCallback.bind(this, sortField, sortType));
 
-    this.buildPackUI(newData);
+  this.buildPackUI(newData);
 };
 
 Packs.prototype.changeSort = function(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    var sortField = jQuery(event.target).parent().data('sort');
-    var sortType = jQuery(event.target).parent().data('sort-type');
+  var sortField = jQuery(event.target).parent().data('sort');
+  var sortType = jQuery(event.target).parent().data('sort-type');
 
-    if (sortField === 'none') {
-        this.buildPackUI(this.data);
-    } else {
-        this.sortPacks(sortField, sortType);
-    }
+  if (sortField === 'none') {
+      this.buildPackUI(this.data);
+  } else {
+      this.sortPacks(sortField, sortType);
+  }
 
-    return false;
+  return false;
 };
 
 var packs = new Packs(data).init();
